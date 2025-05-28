d54716c58311db9b9d9539462b3827ffb03b16ac
Hydrovolter <rionray1234@gmail.com>
2025-05-28 21:30:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# arcora
+This is arcora. A simple way to play music anywhere, at anytime.
+Simply enter a song in the search bar, click it, done.
+
+Oh, did I forget to mention: It's fully static.
+
+i dont feel like writing anything here just figure it out lol
---
+++ js/endpoints.js
@@ -0,0 +1,12 @@
+//const YT_KEY = "AIzaSyDyS3-fRybg6VCIAoRzAt47PNnEVMOsODs";
+// stealing this gives you NO GAIN
+// just make it your own, it's 100% free...
+// AIzaSyAgfWDE5zoRPZWx_BDuCQYFwIIdPOg1M0E
+
+const YT_KEY = "AIzaSyAgfWDE5zoRPZWx_BDuCQYFwIIdPOg1M0E";
+
+
+const LYRIC_EP = "https://api.lyrics.ovh/v1/";
+const YT_EP = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=";
+//const YT_EP = "https://api.hydrovolter.workers.dev/music/?part=snippet&q=";
+const SEARCH_EP = "https://itunes.apple.com/search?term=";
