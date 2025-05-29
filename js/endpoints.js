7d762f89eb3ca98b2fbcc33a8b69bae0fd9d1b74
Hydrovolter <rionray1234@gmail.com>
2025-05-29 12:05:11 +0100

    multi-api cycling

--- js/endpoints.js
+++ js/endpoints.js
@@ -1,9 +1,21 @@
 //const YT_KEY = "AIzaSyDyS3-fRybg6VCIAoRzAt47PNnEVMOsODs";
-// stealing this gives you NO GAIN
-// just make it your own, it's 100% free...
-// AIzaSyAgfWDE5zoRPZWx_BDuCQYFwIIdPOg1M0E
+//const YT_KEY = "AIzaSyAgfWDE5zoRPZWx_BDuCQYFwIIdPOg1M0E";
 
-const YT_KEY = "AIzaSyAgfWDE5zoRPZWx_BDuCQYFwIIdPOg1M0E";
+const YT_KEYS = [
+    'AIzaSyDyS3-fRybg6VCIAoRzAt47PNnEVMOsODs',
+    'AIzaSyAgfWDE5zoRPZWx_BDuCQYFwIIdPOg1M0E'
+];
+
+const RAPIDAPI_HOST = 'youtube-v3-alternative.p.rapidapi.com';
+
+//const RAPIDAPI_KEY = '97ffa9c073mshdf976f56ef861b2p1294d8jsnb5302f45e8d8';
+//const RAPIDAPI_KEY = '77c80e02acmshceeac2836195444p1415aejsn1ab385b54e75';
+const RAPIDAPI_KEYS = [
+    '97ffa9c073mshdf976f56ef861b2p1294d8jsnb5302f45e8d8',
+    '77c80e02acmshceeac2836195444p1415aejsn1ab385b54e75'
+];
+
+
 
 
 const LYRIC_EP = "https://api.lyrics.ovh/v1/";
